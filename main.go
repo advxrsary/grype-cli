d7470c82279917c04b3d4b9a6ca9e483c6adaa84
Sevastian Zare <sevastian.zare@knf.stud.vu.lt>
2022-12-01 01:33:14 +0200

    first steps

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"grype-cli/cli"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/advxrsary/vuln-scanner/cli"
 )
 
 func main() {
-	cli.Run()
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <image>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	image, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Printf("Error getting absolute path for image: %v", err)
+		os.Exit(1)
+	}
+	cli.Cli(image)
 }
